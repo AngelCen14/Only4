641579388b8457fff1a00b73855fa4d12b216d2a
JohnWormwood <127157172+JohnWormwood@users.noreply.github.com>
2024-02-01 11:05:10 +0100

    Wander test

--- scritps/ElBicho.gd
+++ scritps/ElBicho.gd
@@ -8,8 +8,11 @@ var player = null
 var marker = null
 var state_machine
 
+
+
 @export var player_plath : NodePath
 @export var player_plath2 : NodePath
+@export var wander_direction : Node3D
 
 @onready var nav_agent = $NavigationAgent3D
 @onready var anim_tree = $AnimationTree
@@ -21,21 +24,27 @@ func _ready():
 
 
 func _process(delta):
-
 	velocity = Vector3.ZERO
 	var direction = Vector3()
 	
 	match state_machine.get_current_node():
 		"Andar":
 			#Movimiento del bicho, sigue al "target" que es el player
-			#nav_agent.set_target_position(player.global_transform.origin)
+			#nav_agenset_t.target_position(player.global_transform.origin)
 			#var next_nav_point = nav_agent.get_next_path_position()
 			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			nav_agent.set_target_position(marker.global_transform.origin)
+			
+			#Movimiento al marker
+			#nav_agent.set_target_position(marker.global_transform.origin)
+			#var next_nav_point = nav_agent.get_next_path_position()
+			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			
+			nav_agent.set_target_position(wander_direction.direction*200)
 			var next_nav_point = nav_agent.get_next_path_position()
 			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
 			
 			
+			#velocity = wander_direction.direction * 200
 			
 			#Mirar a la posicion que se mueve
 			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
