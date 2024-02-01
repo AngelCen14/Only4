d5ac79d6e3f274c3495c644c4f33f834e1bd38ee
AngelCen14 <angelcen1447@gmail.com>
2024-02-01 12:03:50 +0100

    Bicho arreglado
    SUUUUUUUUUUUUUUUUUUUUUUUU

--- scritps/ElBicho.gd
+++ scritps/ElBicho.gd
@@ -1,65 +1,56 @@
 extends CharacterBody3D
 
-
 const SPEED = 1.8
 const RANGO_ATAQUE = 2
 
-var player = null
-var marker = null
+var markers = []
+var current_marker_index = 0
 var state_machine
+var player = null
 
-
-
-@export var player_plath : NodePath
-@export var player_plath2 : NodePath
-@export var wander_direction : Node3D
-
+@export var markers_group : String
+@export var player_path : NodePath
 @onready var nav_agent = $NavigationAgent3D
 @onready var anim_tree = $AnimationTree
 
 func _ready():
-	player = get_node(player_plath)
-	marker = get_node(player_plath2)
+	# Obtener todos los nodos en el grupo de marcadores
+	markers = get_tree().get_nodes_in_group(markers_group)
 	state_machine = anim_tree.get("parameters/playback")
-
+	player = get_node(player_path)
 
 func _process(delta):
 	velocity = Vector3.ZERO
-	var direction = Vector3()
-	
+
 	match state_machine.get_current_node():
 		"Andar":
-			#Movimiento del bicho, sigue al "target" que es el player
-			#nav_agenset_t.target_position(player.global_transform.origin)
-			#var next_nav_point = nav_agent.get_next_path_position()
-			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			
-			#Movimiento al marker
-			#nav_agent.set_target_position(marker.global_transform.origin)
-			#var next_nav_point = nav_agent.get_next_path_position()
-			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			
-			nav_agent.set_target_position(wander_direction.direction*200)
+			# Moverse hacia el marcador actual
+			nav_agent.set_target_position(markers[current_marker_index].global_transform.origin)
 			var next_nav_point = nav_agent.get_next_path_position()
 			velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-			
-			
-			#velocity = wander_direction.direction * 200
-			
-			#Mirar a la posicion que se mueve
+
+			# Mirar hacia la posición del marcador actual
 			look_at(Vector3(global_position.x + velocity.x, global_position.y, global_position.z + velocity.z), Vector3.UP)
+
+			# Verificar si ha llegado al marcador actual
+			if global_position.distance_to(markers[current_marker_index].global_transform.origin) < 1.0:
+				# Cambiar al siguiente marcador
+				current_marker_index = (current_marker_index + 1) % markers.size()
+				
+			#Movimiento del bicho, sigue al "target" que es el player
+			#nav_agent.set_target_position(player.global_transform.origin)
+			#var next_nav_point = nav_agent.get_next_path_position()
+			#velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+
 		"Atacar":
-			#Mirar a la posicion que se mueve
+			# Mirar hacia la posición del jugador
 			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
-	
-	
-	#Condiciones
+
+	# Condiciones
 	anim_tree.set("parameters/conditions/atacar", _target_in_range())
 	anim_tree.set("parameters/conditions/andar", !_target_in_range())
 
-
 	move_and_slide()
-
 
 func _target_in_range():
 	return global_position.distance_to(player.global_position) < RANGO_ATAQUE
--- scritps/Wander.gd
+++ scritps/Wander.gd
@@ -8,7 +8,6 @@ var current_position : Marker3D
 
 var direction : Vector3 = Vector3.ZERO
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	positions = get_tree().get_nodes_in_group(group_name)
@@ -20,17 +19,16 @@ func _physics_process(delta):
 	if global_position.distance_to(current_position.position) < 10:
 		_get_next_position()
 
-
 func _get_positions():
 	temp_positions = positions.duplicate()
 	temp_positions.shuffle()
-	print("get positions")
+	#print("get positions")
 
 func _get_next_position():
 	if temp_positions.is_empty():
-		print("position empty")
+		#print("position empty")
 		_get_positions()
-	print("position not empty")
+	#print("position not empty")
 	current_position = temp_positions.pop_front()
-	#direction = (current_position.position - global_position).normalized()
-	direction = to_local(current_position.position).normalized()
+	direction = (current_position.position - global_position).normalized()
+
