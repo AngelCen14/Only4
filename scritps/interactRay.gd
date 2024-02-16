899398482fc9878a589a453883a6322cc1c2234f
JohnWormwood <juan.ajenjo13@gmail.com>
2024-02-16 17:24:31 +0100

    llaves full

--- scritps/interactRay.gd
+++ scritps/interactRay.gd
@@ -2,21 +2,28 @@ extends RayCast3D
 
 @onready var label = $Label
 @onready var player_llaves = $"../../.."
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_exception(owner)
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if is_colliding():
-		if(get_collider().name=="Key" || get_collider().name=="Salida"):
+		var str: String = get_collider().name
+		if(get_collider().name.contains("key")):
 			$Label.visible=true
 			if Input.is_action_just_pressed("take"):
 				player_llaves.llaves+=1
-				get_node("../Terreno/key").visible=false
-				print($"../../..".llaves)
+				var keyname= "../Terreno/"+get_collider().get_parent().get_parent().get_parent().get_parent().name
+				player_llaves.get_llaves(keyname)
+				print(get_collider().name)
+		elif (get_collider().name=="Salida"):
+			if player_llaves.llaves>=4:
+				print("winer winer chiken diner")
+			else :
+				print("te falta odio...")
 		else :
 			$Label.visible=false
 		
